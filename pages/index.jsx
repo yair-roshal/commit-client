1d5b6c0d9bac92d47bf7d433d8555ed462fb802d
yair-roshal <yair7705778@gmail.com>
2024-02-17 22:07:49 +0200

    update 1 file and delete 1 file

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,13 @@
-// import { useEffect } from "react"
-// import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function Home() {
-  // const router = useRouter()
+  const router = useRouter()
 
-  // useEffect(() => {
-  //   // Redirect the user to the form page at application startup
-  //   router.push("/form")
-  // }, [])
+  useEffect(() => {
+    // Redirect the user to the form page at application startup
+    router.push("/form")
+  }, [])
 
-  return (
-    <>
-      <h1>Home page</h1>
-    </>
-  )
+  return <> </>
 }

8921dd170683926100c6905418455c0f290be7bd
yair-roshal <yair7705778@gmail.com>
2024-02-17 21:59:07 +0200

    update pages/index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react"
-import { useRouter } from "next/router"
+// import { useEffect } from "react"
+// import { useRouter } from "next/router"
 
 export default function Home() {
-  const router = useRouter()
+  // const router = useRouter()
 
-  useEffect(() => {
-    // Redirect the user to the form page at application startup
-    router.push("/form")
-  }, [])
+  // useEffect(() => {
+  //   // Redirect the user to the form page at application startup
+  //   router.push("/form")
+  // }, [])
 
-  return <> </>
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  )
 }
